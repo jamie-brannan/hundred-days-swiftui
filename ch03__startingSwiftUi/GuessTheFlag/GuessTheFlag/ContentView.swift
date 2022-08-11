9210c9cb6250499d5e6e917df4ec0131c6432df5
Jamie Brannan <jamiebrannan2@gmail.com>
2022-08-11 17:39:32 +0200

    [project_2] init and first v stack

---
+++ ch03__startingSwiftUi/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jamie Marie Brannan on 11/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack(alignment: .leading) {
+        Text("Hello, world!")
+        Text("This is inside a stack")
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
