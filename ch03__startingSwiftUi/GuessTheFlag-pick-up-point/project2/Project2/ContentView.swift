02ec1439f7d0da5fb62dbdd114cb1259c5cc3e27
Jamie Brannan <jamiebrannan2@gmail.com>
2025-01-02 13:26:34 +0100

    022: Lint

--- ch03__startingSwiftUi/GuessTheFlag-pick-up-point/project2/Project2/ContentView.swift
+++ ch03__startingSwiftUi/GuessTheFlag-pick-up-point/project2/Project2/ContentView.swift
@@ -9,68 +9,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
-
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
-
-    var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                }
-
-                ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
-                    }
-                }
-
-                Spacer()
-            }
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  /// This is the answer to the question and the country that'll be selected
+  @State private var correctAnswer = Int.random(in: 0...2)
+  
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  
+  var body: some View {
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
+      
+      VStack(spacing: 30) {
+        VStack {
+          Text("Tap the flag of")
+            .foregroundColor(.white)
+          
+          Text(countries[correctAnswer])
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .fontWeight(.black)
         }
-        .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
-                self.askQuestion()
-            })
+        
+        ForEach(0 ..< 3) { number in
+          Button(action: {
+            self.flagTapped(number)
+          }) {
+            Image(self.countries[number])
+              .renderingMode(.original)
+              .clipShape(Capsule())
+              .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+              .shadow(color: .black, radius: 2)
+          }
         }
+        
+        Spacer()
+      }
     }
-
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreTitle = "Correct"
-        } else {
-            scoreTitle = "Wrong"
-        }
-
-        showingScore = true
+    .alert(isPresented: $showingScore) {
+      Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+        self.askQuestion()
+      })
     }
-
-    func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+  }
+  
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+    } else {
+      scoreTitle = "Wrong"
     }
+    
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
